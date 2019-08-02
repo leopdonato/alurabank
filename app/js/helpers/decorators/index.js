187628e231e43110b8b697b9c50ea281eaabff01
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-08-02 17:57:54 -0300

    Criando decorator DOM Inject para lazy loading

--- app/js/helpers/decorators/index.js
+++ app/js/helpers/decorators/index.js
@@ -1,4 +1,4 @@
-System.register(["./logarTempoExecucao"], function (exports_1, context_1) {
+System.register(["./logarTempoExecucao", "./domInject"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     function exportStar_1(m) {
@@ -12,6 +12,9 @@ System.register(["./logarTempoExecucao"], function (exports_1, context_1) {
         setters: [
             function (logarTempoExecucao_1_1) {
                 exportStar_1(logarTempoExecucao_1_1);
+            },
+            function (domInject_1_1) {
+                exportStar_1(domInject_1_1);
             }
         ],
         execute: function () {
