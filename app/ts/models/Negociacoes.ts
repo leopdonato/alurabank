b4cb45d2325fbd05d57cdd7a1655f2ae45a1e54a
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-08-07 18:13:07 -0300

    Polimorfismo com classe para fazer a impressao de dados no console

--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -1,6 +1,7 @@
 import { Negociacao } from "./Negociacao";
+import { Imprimivel } from "./Imprimivel";
 
-export class Negociacoes {
+export class Negociacoes extends Imprimivel {
 
     private _negociacoes: Negociacao[] = [];
 
@@ -11,5 +12,11 @@ export class Negociacoes {
     paraArray(): Negociacao[] {
 
         return ([] as Negociacao[]).concat(this._negociacoes);
+    }
+
+    paraTexto(): void {
+
+        console.log('-- paraTexto --');
+        console.log(JSON.stringify(this._negociacoes));
     }
 }
